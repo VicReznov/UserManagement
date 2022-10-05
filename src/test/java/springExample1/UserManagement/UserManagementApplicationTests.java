3f50f435170076deea6c536342715549429d8b7a
Reznov <reznov03048@gmail.com>
2022-10-05 21:40:52 +0900

    Initial commit User Repository 테스트 케이스 작성하기

---
+++ src/test/java/springExample1/UserManagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package springExample1.UserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
