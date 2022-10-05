b542a4e4047a59b76ff11b600c9fc434e5aea9bf
Reznov <reznov03048@gmail.com>
2022-10-05 22:08:43 +0900

    add MemberService class

---
+++ src/main/java/springExample1/UserManagement/service/MemberService.java
@@ -0,0 +1,36 @@
+package springExample1.UserManagement.service;
+
+import springExample1.UserManagement.domain.Member;
+import springExample1.UserManagement.repository.MemberRepository;
+import springExample1.UserManagement.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원가입 메소드
+    public Long join(Member member){
+        // 이름이 중복인 회원은 가입 불가
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    // 회원 한 명 찾기
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
