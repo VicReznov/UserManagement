8bb72cb9fb510e87349a9c02f176b4e7a0f3d42c
Reznov <reznov03048@gmail.com>
2022-10-06 19:51:14 +0900

    홈 화면, 회원 등록 기능 추가

--- src/main/java/springExample1/UserManagement/controller/MemberController.java
+++ src/main/java/springExample1/UserManagement/controller/MemberController.java
@@ -2,6 +2,9 @@ package springExample1.UserManagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import springExample1.UserManagement.domain.Member;
 import springExample1.UserManagement.service.MemberService;
 
 @Controller
@@ -13,5 +16,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
