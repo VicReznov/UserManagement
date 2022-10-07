13c258ef856f05ec01dee2d198d3eca42eff0e77
Reznov <reznov03048@gmail.com>
2022-10-07 22:06:46 +0900

    Jdbc 사용해서 db 연결

--- src/main/java/springExample1/UserManagement/SpringConfig.java
+++ src/main/java/springExample1/UserManagement/SpringConfig.java
@@ -1,21 +1,37 @@
 package springExample1.UserManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springExample1.UserManagement.repository.JdbcMemberRepository;
 import springExample1.UserManagement.repository.MemberRepository;
 import springExample1.UserManagement.repository.MemoryMemberRepository;
 import springExample1.UserManagement.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
+    /*
+    Memory -> Jdbc
+     */
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
